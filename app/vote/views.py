046cfb647b8f57db04e59d81d6c7632134e93295
dwisulfahnur <dwisulfahnur@gmail.com>
2016-01-06 14:10:35 +0800

    change route method in question view

--- app/vote/views.py
+++ app/vote/views.py
@@ -9,7 +9,7 @@ vote_views = Blueprint('vote', __name__, template_folder='../templates', static_
 @vote_views.route('/', methods=['GET', 'POST'])
 def inputID():
     if 'user_id' in session:
-        return redirect(url_for('.question', id=1))
+        return redirect(url_for('.question'))
     form = InputID()
     #handle POST method
     if form.validate_on_submit():
@@ -19,23 +19,21 @@ def inputID():
             db.session.add(user)
             db.session.commit()
             session['user_id'] = user.id
-            return redirect(url_for('.question', id=1))
+            return redirect(url_for('.question'))
         form.email.errors.append("Email has been registered")
     #handle GET method
     return render_template('inputID.html', form=form)
 
 
 #vote route
-@vote_views.route('/question/', defaults={'id':1}, methods=['POST', 'GET'])
-@vote_views.route('/question/<id>', methods=['POST', 'GET'])
-def question(id):
+@vote_views.route('/question/', methods=['POST', 'GET'])
+def question():
     #check session
     if "user_id" not in session:
         flash("You're not registered, Enter your date if you wanto register")
         return redirect(url_for('.inputID'))
 
     ####Handle question view####
-    question = Question.query.get(id)
     for x in Question.query.all():
         if not User_answer.query.filter_by(user_id=session['user_id'], question_id=x.id).first():
             question = Question.query.get(x.id)
@@ -49,7 +47,7 @@ def question(id):
         else: answer = 0
         db.session.add(User_answer(session["user_id"], question.id, answer))
         db.session.commit()
-        return redirect(url_for('.question', id=1))
+        return redirect(url_for('.question'))
 
     ### HANDLE GET METHOD ###
     return render_template('question.html', question=question)
@@ -61,7 +59,7 @@ def thanks():
         #check count the number of questions answered
         if Question.query.count() is not User_answer.query.filter_by(user_id=session['user_id']).count():
             flash("You must answer all question!")
-            return redirect(url_for('.question', id=1))
+            return redirect(url_for('.question'))
         #remove all session
         session.clear()
     else:
